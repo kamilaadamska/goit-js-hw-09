cbcef63f77fe922878791ebb538ba444ac7ac2f9
Kamila Adamska <121kama121@gmail.com>
2023-04-17 00:29:23 +0200

    Finished task 01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,20 @@
+const buttonStartEl = document.querySelector('button[data-start]');
+const buttonStopEl = document.querySelector('button[data-stop]');
+const bodyEl = document.querySelector('body');
+let changeColorId = null;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+buttonStartEl.addEventListener('click', () => {
+  changeColorId = setInterval(() => {
+    bodyEl.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  buttonStartEl.setAttribute('disabled', '');
+});
+
+buttonStopEl.addEventListener('click', () => {
+  clearInterval(changeColorId);
+  buttonStartEl.removeAttribute('disabled');
+});
