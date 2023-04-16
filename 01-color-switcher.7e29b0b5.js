c71d1625315a8373f4b1e7622b562bf5d5813fd2
kamilaadamska <117036552+kamilaadamska@users.noreply.github.com>
2023-04-16 22:29:45 +0000

    Deploying to gh-pages from @ kamilaadamska/goit-js-hw-09@cbcef63f77fe922878791ebb538ba444ac7ac2f9 🚀

---
+++ 01-color-switcher.7e29b0b5.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body"),o=null;t.addEventListener("click",(function(){o=setInterval((function(){n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),t.setAttribute("disabled","")})),e.addEventListener("click",(function(){clearInterval(o),t.removeAttribute("disabled")}))}();
+//# sourceMappingURL=01-color-switcher.7e29b0b5.js.map
